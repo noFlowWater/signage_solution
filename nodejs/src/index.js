4158d84a4269a33f3350e27dd098d265f0a27f26
bokyeong <wntjdals0412@gmail.com>
2023-11-06 03:40:05 +0900

    add : admin login, db connect 관리자 로그인 기능, 데이터베이스 연동 완료

---
+++ nodejs/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const port = 4000
+const database = require('./database')
+
+app.use(express.json());
+
+//db 연결
+database.$connect()
+    .then(() => {
+        console.log('연결완료');
+    })
+    .catch(err => {
+        console.log(err);
+    } )
+
+//admin 관련 api 요청은 routes/admin 에서 처리
+app.use('/admin', require('./routes/admin'));
+
+// // '/menu' 관련 api 요청은 routes/menu에서 처리
+// app.use('/menu', require('./routes/menu'))
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500); //express에서 제공하는 errorstatus가 있다면 그걸 클라이언트한테 보내주고 없다면 임의로 500을 보낸다
+    res.send(error.message || '서버에서 에러가 났습니다'); //지정한 error msg가 있다면 그걸 전달하고 없다면 해당 메시지를 보낸다
+})
+
+app.listen(4000, () => {
+    console.log(`${port}번에서 실행이 되었습니다.`);
+})
