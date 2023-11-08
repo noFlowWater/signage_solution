4832ff99c5ab06be1690fc20365edd8db28aabbb
jinno Yun <jinno321@naver.com>
2023-11-08 21:13:19 +0900

    [feat] : 카테고리 별로 메뉴리스트 보기 라우터 구현 완료 log 출력으로 테스트 코드 까지 구현함 예시 데이터가 없어서 테스트 해보진 않음

--- nodejs/src/routes/menu.js
+++ nodejs/src/routes/menu.js
@@ -20,15 +20,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 
-//추천 메뉴리스트 보기
-/*router.get('/menu/recommend', async(req,res,error) => {
-    
-})*/
+/*//추천 메뉴리스트 보기 
+router.get('/menu/recommend', async(req,res,error) => {
+    git /menu/recommend
+})*/ //<- 이거는 알고리즘이 필요하기 때문에 조금 더 생각을 해 보아요.
 
 //선택한 메뉴 상세보기
 router.get('/:menuID', async(req,res,error) => { //<- :menuID는 req.params.menuID내에 존재
     const thismenuID = req.params.menuID; //menuID를 가져와서
-    console.log("menuID : ",menuID);
+    console.log("menuID : ",thismenuID);
 
     //DB에서 menuID 매칭후 추출
     const result = await prisma.Menu.findUnique({
@@ -44,6 +44,7 @@ router.get('/:menuID', async(req,res,error) => { //<- :menuID는 req.params.menu
             }
         }
     });
+
     console.log("menu_name : ",result.menu_name);
     console.log("menu_description : ",result.menu_description);
     console.log("price : ",result.price);
@@ -51,6 +52,31 @@ router.get('/:menuID', async(req,res,error) => { //<- :menuID는 req.params.menu
     //res.json() 해서 메뉴표시에 필요한것들 보내주면 된다.
     res.json(result);
 })
+
+//카테고리 별로 메뉴리스트보기
+router.get('/category/:categoryID', async(req,res,error) => {
+    const thisCategoryID = req.params.categoryID; //categoryID를 가져와서
+    console.log("categoryID : ",thisCategoryID);
+
+    const result = await prisma.Menu.findMany({
+        where: {
+            categoryId: thisCategoryID
+        },
+        select: {
+            menuID: true,
+            menu_name: true,
+            price: true,
+            file_path: true
+        }
+    })
+
+    console.log("menuID : ",result.menuID);
+    console.log("menu_name : ",result.menu_name);
+    console.log("price : ",result.price);
+    console.log("file_path : ",result.file_path);
+    //res.json() 해서 메뉴표시에 필요한것들 보내주면 된다.
+    res.json(result);
+});
 
 
 module.exports = router;
