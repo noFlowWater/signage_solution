89a83d8795a9f6b539f3fcc5c531d4f544523cfa
jinno Yun <jinno321@naver.com>
2023-11-19 10:34:01 +0900

    [FEAT]:admin 카테고리별 메뉴 보기 구현

--- nodejs/src/routes/admin.js
+++ nodejs/src/routes/admin.js
@@ -23,4 +23,32 @@ router.post('/login', async(req,res,error) => {
     }
 }) 
 
+//관리자 카테고리별 메뉴
+router.get('/', async(req,res,error) => {
+    const thisCategoryID = req.body.categoryID; //categoryID를 가져와서
+    console.log("categoryID : ",thisCategoryID);
+
+    //카테고리 같은거 추출후 보내줌.
+    const result = await prisma.Menu.findMany({
+        where: {
+            categoryId: thisCategoryID
+        },
+        select: {
+            menuID: true,
+            menu_name: true,
+            price: true,
+            file_path: true,
+            is_soldout: true
+        }
+    })
+
+    console.log("menuID : ",result.menuID);
+    console.log("menu_name : ",result.menu_name);
+    console.log("price : ",result.price);
+    console.log("file_path : ",result.file_path);
+    console.log("is_soldout : ",result.is_soldout);
+    //res.json() 해서 메뉴표시에 필요한것들 보내주면 된다.
+    res.json(result);
+});
+
 module.exports = router;
