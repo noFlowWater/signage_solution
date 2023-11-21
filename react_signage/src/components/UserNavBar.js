716c72874f741164ed82f431f68c3a9ab3552536
MINSU LEE <minsoo030232@gmail.com>
2023-11-21 13:52:02 +0900

    feat: 사용자 메뉴페이지 NavBar 컴포넌트 수정 및 서버 연동 코드 추가

--- react_signage/src/components/UserNavBar.js
+++ react_signage/src/components/UserNavBar.js
@@ -20,7 +20,7 @@ const UserNavBar = () => {
                         activeClassName = "active"
                         className="nav-link" 
                         aria-current="page" 
-                        to="/"
+                        to="/user/menu/0"
                         style={{ textDecoration: 'none' }}>
                         추천</NavLink>
                     </li>
@@ -30,7 +30,7 @@ const UserNavBar = () => {
                         activeClassName = "active"
                         className="nav-link" 
                         aria-current="page" 
-                        to="/"
+                        to="/user/menu/1"
                         style={{ textDecoration: 'none' }}>
                         김밥</NavLink>
                     </li>
@@ -40,7 +40,7 @@ const UserNavBar = () => {
                         activeClassName = "active"
                         className="nav-link" 
                         aria-current="page" 
-                        to="/"
+                        to="/user/menu/2"
                         style={{ textDecoration: 'none' }}>
                         라면</NavLink>
                     </li>
@@ -49,18 +49,27 @@ const UserNavBar = () => {
                         activeClassName = "active"
                         className="nav-link" 
                         aria-current="page" 
-                        to="/"
+                        to="/user/menu/3"
                         style={{ textDecoration: 'none' }}>
                         떡볶이</NavLink>
+                    </li>
+                    <li className="nav-item me-5">
+                    <NavLink 
+                        activeClassName = "active"
+                        className="nav-link" 
+                        aria-current="page" 
+                        to="/user/menu/4"
+                        style={{ textDecoration: 'none' }}>
+                        돈가스</NavLink>
                     </li>
                     <li className="nav-item">
                     <NavLink 
                         activeClassName = "active"
                         className="nav-link" 
                         aria-current="page" 
-                        to="/"
+                        to="/user/menu/5"
                         style={{ textDecoration: 'none' }}>
-                        음료</NavLink>
+                        사이드</NavLink>
                     </li>
                 </ul> 
             </div>
--- react_signage/src/pages/UserMenuPage.js
+++ react_signage/src/pages/UserMenuPage.js
@@ -1,6 +1,35 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import UserNavBar from "../components/UserNavBar";
 
 const UserMenuPage = () => {
+    const { id } = useParams(); // URL에서 id 값을 추출
+    const [menus, setMenus] = useState([]); // 메뉴 정보를 저장할 state
+
+    // useEffect(() => {
+    //     // 서버에서 메뉴 정보를 불러오는 함수
+    //     const fetchMenus = async () => {
+    //         const response = await axios.get(`your_server_url/menu/${id}`);
+    //         setMenus(response.data);
+    //     }
+
+    //     fetchMenus();
+    // }, [id]); // id 값이 변경될 때마다 서버에서 메뉴 정보를 다시 불러옴
+
+    // return (
+    //     <div>
+    //         <UserNavBar />
+
+    //         {menus.map((menu, index) => (
+    //             <div key={index}>
+    //                 <h1>{menu.name}</h1>
+    //                 <p>{menu.price}</p>
+    //                 <img src={menu.image} alt={menu.name} />
+    //             </div>
+    //         ))}
+    //     </div>
+    // );
     return (
         <div>
             <UserNavBar />
