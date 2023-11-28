767fd785fad4da82632517659c1c98df4ebc9f5a
MINSU LEE <minsoo030232@gmail.com>
2023-11-28 21:16:41 +0900

    feat: 사용자 주문하기 페이지 생성 및 UserOrderNavBar 추가

--- react_signage/src/pages/UserOrderPage.js
+++ react_signage/src/pages/UserOrderPage.js
@@ -1,6 +1,11 @@
+import UserOrderNavBar from "../components/UserOrderNavBar";
+
 const UserOrderPage = () => {
     return (
+        <div>
+        <UserOrderNavBar />
         <div>주문하기 페이지</div>
+        </div>
     )
 }
 
